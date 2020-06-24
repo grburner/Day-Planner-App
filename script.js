cf5a95505ac0cebc4c3edfa8e722c766d6c6b59f
Gary Burner <grburner@gmail.com>
2020-06-24 13:04:07 -0400

    started local storage function

--- script.js
+++ script.js
@@ -18,10 +18,22 @@ $(document).ready(function() {
         });  
     });
 
+function setTaskValues() {
+    console.log('into setTaskValues')
+    // $.each(localStorage, function(key, value) {
+    // }); ******* use each to get this done *********
+    for (var i = 0; i < localStorage.length; i++) {
+        //$(localStorage.key(i)).each(function() {
+            console.log('local storage key')
+            console.log(localStorage.getItem(localStorage.key(i)));
+    };
+};
+
 function saveUserInput() {
     var savedInput = $(this).siblings(".input-field").text()
     var savedTimeSlot = $(this).siblings(".hour").text()
     localStorage.setItem(savedTimeSlot, savedInput)
+    setTaskValues()
 };
 // build input function to take user input and save it to localStorage
 $(document).on("click", ".save-btn", saveUserInput)
