a685e3609e3162711b4b3a40c1e808e025f9892b
Gary Burner <grburner@gmail.com>
2020-06-24 14:04:23 -0400

    fill in event fields on start

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var currentDate = moment().format('YYYY-MM-DD')
 
 // divs are generated run a setTimeout loop that constantly checks the time and updates the grid color
 $(document).ready(function() {
+    setTaskValues()
     $("#timeSlotContainer").children().each(function() {
-//          if ( moment().isBefore(moment($(this).data("timeSlot"))._i, "HH:mm") ) {
             if ( moment().isBefore(currentDate + ' ' + $(this).data("timeSlot")) ) {
                 console.log('isbefore ' + $(this).data("timeSlot"))
                 $(this).addClass("future")
@@ -22,11 +22,21 @@ function setTaskValues() {
     console.log('into setTaskValues')
     // $.each(localStorage, function(key, value) {
     // }); ******* use each to get this done *********
-    for (var i = 0; i < localStorage.length; i++) {
-        var displayKey = localStorage.getItem(localStorage.key(i))
-
+    for ( var i = 0; i < localStorage.length; i++ ) {
+        var displayValue = localStorage.getItem(localStorage.key(i))
+        var displayKey = localStorage.key(i)
+        compareStorage(displayKey, displayValue)
     };
 };
+
+function compareStorage(key, eventVal) {
+    $(".time-slot").each(function() {
+        console.log('timeslot ' + $(this).data("timeSlot") + ' key: ' + key)
+        if ( $(this).data("timeSlot") === key) {
+            $(this).children(".input-field").val(eventVal)
+        };
+    }
+)};
 
 function saveUserInput() {
     var savedInput = $(this).siblings(".input-field").val()
