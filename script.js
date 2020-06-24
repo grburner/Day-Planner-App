b9d2536f7745ca79b0ef93b951facf09228163eb
Gary Burner <grburner@gmail.com>
2020-06-24 12:13:30 -0400

    started storage function;

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ $(document).ready(function() {
         });  
     });
 
-
+function saveUserInput() {
+    var savedInput = $(this).siblings(".input-field").text()
+    var savedTimeSlot = $(this).siblings(".hour").text()
+    localStorage.setItem(savedTimeSlot, savedInput)
+};
 // build input function to take user input and save it to localStorage
+$(document).on("click", ".save-btn", saveUserInput)
     // build save button functionality
