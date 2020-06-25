e79d205b5001530ce0316efa348930fac7c2a5d5
Gary Burner <grburner@gmail.com>
2020-06-25 08:17:50 -0400

    added comments

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var currentTime = moment().format('LT')
 var currentDate = moment().format('YYYY-MM-DD')
+//variable passed into the .subtract method to decrement by an hour
 var time = moment.duration("01:00", 'HH');
-testMoment = moment('10:10', 'HH:mm')
+//test variable to test outside of 9-5 hours
+var testMoment = moment('10:10', 'HH:mm')
 
-// divs are generated run a setTimeout loop that constantly checks the time and updates the grid color
+//runs setTaskValues to reset the displayed taks
+// -> loops through each time slot to determine if the time slot is before, during, or after the current time, then sets formatting
+// ---> TO DO: wrap in a setTimeout function to run the loop every minute to dynamically update the formatting
 $(document).ready(function() {
     setTaskValues()
     $("#timeSlotContainer").children().each(function() {
@@ -25,6 +29,7 @@ $(document).ready(function() {
     });  
 });
 
+//for each k/v pair in localstorage save the key and value to a variable then pass each into the compareStorage() function to determine which time slot to add them to
 function setTaskValues() {
     // $.each(localStorage, function(key, value) {
     // }); ******* use each to get this done *********
@@ -35,6 +40,8 @@ function setTaskValues() {
     };
 };
 
+//compares local storage key to the data-time-slot of each time slot 
+// -> if they match then the value is added to the input field
 function compareStorage(displayKey, displayValue) {
     $(".time-slot").each(function() {
         if ( $(this).data("timeSlot") === displayKey) {
@@ -43,6 +50,7 @@ function compareStorage(displayKey, displayValue) {
     }
 )};
 
+//pulls the input and data-time-slot from the click event and saves them as a key/value pair in local storage
 function saveUserInput() {
     var savedInput = $(this).siblings(".input-field").val()
     var savedTimeSlot = $(this).parent().data("timeSlot")
@@ -50,4 +58,6 @@ function saveUserInput() {
     setTaskValues()
 };
 
+//click event for the .save-btn class
+// -> callback function saveUserInput to save the k/v pair to local storage
 $(document).on("click", ".save-btn", saveUserInput)
