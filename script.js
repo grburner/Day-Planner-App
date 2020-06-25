47919dba0790d289e6b501690dda415eb21cbab3
Gary Burner <grburner@gmail.com>
2020-06-25 11:36:00 -0400

    cleaned and commented, added running date and time functions, updated formatting function to not use moment methods

--- script.js
+++ script.js
@@ -1,52 +1,48 @@
-//var currentTime = moment().format('LT')
-var currentDate = moment().format('YYYY-MM-DD')
-//variable passed into the .subtract method to decrement by an hour
-var time = moment.duration("01:00", 'HH');
-//test variable to test outside of 9-5 hours
-var testMoment = moment('10:59', 'HH:mm')
 
-// ---> TO DO: add the current date in the header
 // ---> TO DO: clean up formatting 
-//      -> add in disk button for the save button and format to blue
+//      -> center disk
+// ---> TO DO: fix text box focus bug
+// ---> TO DO: check that text box formatting updates on the hour
 
 // ---> EXTRAS TO DO:
 //      -> break up the task row into blocks depending on how many tasks are in that time slot
 //      -> make functionality to mark a task complete and move it to a completed tasks list
 //      -> set up the header like my current homepage
 
-//runs setTaskValues to reset the displayed taks
-// -> loops through each time slot to determine if the time slot is before, during, or after the current time, then sets formatting
-// ---> TO DO: wrap in a setTimeout function to run the loop every minute to dynamically update the formatting
-
+// set the date/time & time block formatting on document.ready
 $(document).ready(function() {
-//     setInterval(setTimeblockFormatting, 1000)
+    setTimeblockFormatting()
+    setCurrentTime()
 });
-setTimeblockFormatting()
-//
+
+// updates the date and time function every second
+setInterval(setTimeblockFormatting, 1000)
+setInterval(setCurrentTime, 1000)
+
+// gets the current moment for day and time
+function setCurrentTime() {
+    $("#currentDay").text(moment().format('LL'))
+    $("#currentTime").text(moment().format('LTS'))
+};
+
+// updates the time block formatting by comparing time block's data attribute and comparing it to past, present, or future and setting the class for that formatting
 function setTimeblockFormatting() {
     setTaskValues()
     $("#timeSlotContainer").children().each(function() {
-        var thisData = $(this).data("timeSlot")
-        var thisMoment = moment(thisData, 'HH:mm')
-        var subtractMoment = thisMoment.clone().subtract(time)
-        console.log(Boolean(moment().isAfter(currentDate + ' ' + thisData)))
-        console.log(Boolean(moment().isBefore(currentDate + ' ' + subtractMoment)))
-        if ( moment().isBefore(currentDate + ' ' + thisData) ) {
-            $(this).children(".input-field").addClass("future")
-        //} else if ( moment().isBetween(currentDate + ' ' + thisData, currentDate + ' ' + subtractMoment)) {
-        } else if ( moment().isAfter(currentDate + ' ' + thisData) && moment().isBefore(currentDate + ' ' + subtractMoment) ) {
+        var thisData = parseInt($(this).data('timeSlot').split(":")[0]);
+        const thisMoment = moment().hours()
+        if ( thisData < thisMoment ) {
+            $(this).children(".input-field").addClass("past")
+        } else if ( thisData === thisMoment ) {
             $(this).children(".input-field").addClass("present")
         } else {
-            $(this).children(".input-field").addClass("past")
-            // add in future block
-        };
-    }); 
+            $(this).children(".input-field").addClass("future")
+        }
+    });
 };
 
 //for each k/v pair in localstorage save the key and value to a variable then pass each into the compareStorage() function to determine which time slot to add them to
 function setTaskValues() {
-    // $.each(localStorage, function(key, value) {
-    // }); ******* use each to get this done *********
     for ( var i = 0; i < localStorage.length; i++ ) {
         var displayValue = localStorage.getItem(localStorage.key(i))
         var displayKey = localStorage.key(i)
@@ -58,7 +54,7 @@ function setTaskValues() {
 // -> if they match then the value is added to the input field
 function compareStorage(displayKey, displayValue) {
     $(".time-slot").each(function() {
-        if ( $(this).data("timeSlot") === displayKey) {
+        if ( $(this).data("timeSlot") === displayKey ) {
             $(this).children(".input-field").val(displayValue)
         };
     }
@@ -74,4 +70,26 @@ function saveUserInput() {
 
 //click event for the .save-btn class
 // -> callback function saveUserInput to save the k/v pair to local storage
-$(document).on("click", ".save-btn", saveUserInput)
+$(document).on("click", ".saveBtn", saveUserInput)
+
+
+
+
+/* future: get the moment methods working:
+
+        // var thisData = $(this).data("timeSlot")
+        // var thisMoment = moment(thisData, 'HH:mm')
+        // var subtractMoment = thisMoment.clone().subtract(time)
+        // console.log(Boolean(moment().isAfter(currentDate + ' ' + thisData)))
+        // console.log(Boolean(moment().isBefore(currentDate + ' ' + subtractMoment)))
+        // if ( moment().isBefore(currentDate + ' ' + thisData) ) {
+        //     $(this).children(".input-field").addClass("future")
+        // //} else if ( moment().isBetween(currentDate + ' ' + thisData, currentDate + ' ' + subtractMoment)) {
+        // } else if ( moment().isAfter(currentDate + ' ' + thisData) && moment().isBefore(currentDate + ' ' + subtractMoment) ) {
+        //     $(this).children(".input-field").addClass("present")
+        // } else {
+        //     $(this).children(".input-field").addClass("past")
+        //     // add in future block
+        // };
+
+*/
